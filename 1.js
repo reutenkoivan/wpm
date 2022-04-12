86d5dbd7e6f145b6279ef2f893e4ded117d9376c
Ivan Reutenko <reutenkoivan@gmail.com>
2022-04-12 14:08:18 +0300

    logger

--- 1.js
+++ 1.js
@@ -1,10 +1,19 @@
 // https://www.npmjs.com/package/read-yaml-file
 const path = require('path')
+const { Signales } = require('signales')
 const parseConfig = require('read-yaml-file').default
 
 const configPath = path.resolve('wpm.config.yml')
 
-parseConfig(configPath).then(config => {
-  // eslint-disable-next-line no-console
-  console.log(JSON.stringify(config, null, 2))
+const logger = new Signales({
+  scope: 'wpm',
 })
+
+parseConfig(configPath)
+  .then(config => {
+    // eslint-disable-next-line no-console
+    logger.debug(JSON.stringify(config, null, 2))
+  })
+  .catch(e => {
+    logger.error(`${e.reason.trim()}!\n\n`, e.mark.snippet.trim())
+  })
